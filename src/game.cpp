cfac66fb5d87e74be245d56cf23fa7bcf8effe4f
tr4shp4ndu <46363873+Tr4shP4ndu@users.noreply.github.com>
2024-10-05 02:31:28 +0100

    moving sideways aliens

--- src/game.cpp
+++ src/game.cpp
@@ -112,6 +112,14 @@ void Game::MoveAliens()
 {
   for(auto& alien: aliens)
   {
+    if(alien.position.x + alien.alienImages[alien.type - 1].width > GetScreenWidth())
+    {
+      aliensDirection = -1;
+    }
+    if(alien.position.x < 0)
+    {
+      aliensDirection = 1;
+    }
     alien.Update(aliensDirection);
   }
 }
